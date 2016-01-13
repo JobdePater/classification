99c8bf6a78140fa78a6aa25fbc5d6bdff9db9a25
GertS <gertsterenborg@gmail.com>
2016-01-13 15:30:44 +0100

    added water mask data

--- R/loadData.R
+++ R/loadData.R
@@ -12,10 +12,14 @@ dir.create('data',showWarnings = F)
 load("data/GewataB1.rda")
 load("data/GewataB5.rda")
 load("data/GewataB7.rda")
-
+load("data/vcfGewata.rda")
 
 # Brick data --------------------------------------------------------------
 
+vcfGewata[vcfGewata > 100] <- NA ##values above 100 is probably water
+alldata <- brick(GewataB1, GewataB5, GewataB7, vcfGewata)
+names(alldata) <- c("band1", "band5", "band7", "VCF" )
 
-alldata <- brick(GewataB1, GewataB5, GewataB7)
-names(alldata) <- c("band1", "band5", "band7")
+## extract all data to a data.frame
+df <- as.data.frame(getValues(alldata))
+
--- main.R
+++ main.R
@@ -1,3 +1,5 @@
 ## Job de Pater & Gert Sterenborg
 # 13-01-2016
 #
+
+source("R/loadData.R")
