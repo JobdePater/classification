c30813a3894df5c7d6a7521ab9611b85df70d828
GertS <gertsterenborg@gmail.com>
2016-01-13 16:40:08 +0100

    createFigs function

--- R/createFigs.R
+++ R/createFigs.R
@@ -4,5 +4,7 @@ library(grid)
 dir.create('figs',showWarnings = F)
 
 createFigs <- function(alldataBrick,maxpixels=1000){
+  png(filename = 'figs/pairs.png')
   pairs <- pairs(alldataBrick,maxpixels=5000)
+  dev.off()
 }
