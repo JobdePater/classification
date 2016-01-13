219ed6d874e8ecd9649711433505503c292856fb
GertS <gertsterenborg@gmail.com>
2016-01-13 15:19:52 +0100

    brick data

--- R/loadData.R
+++ R/loadData.R
@@ -12,3 +12,10 @@ dir.create('data',showWarnings = F)
 load("data/GewataB1.rda")
 load("data/GewataB5.rda")
 load("data/GewataB7.rda")
+
+
+# Brick data --------------------------------------------------------------
+
+
+alldata <- brick(GewataB1, GewataB5, GewataB7)
+names(alldata) <- c("band1", "band5", "band7")
